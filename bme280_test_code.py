4d959284b774d40942e4663d82a8780e0d63eeee
xiaolinyang927 <54749402+xiaolinyang927@users.noreply.github.com>
2019-12-08 17:05:44 -0500

    Update bme280_test_code.py

--- bme280_test_code.py
+++ bme280_test_code.py
@@ -1,16 +1,17 @@
-  import bme280
-  import smbus2
-  from time import sleep
-  
-  port = 1
-  address = 0x77
-  bus = smbus2.SMBus(port)
-  
-  bme280.load_calibration_params(bus, address)
-  
-  def get bme280data:
-    bme280_data = bme280.sample(bus, address)
-    print("temperature: %0.2f C" % (bme280_data.temperature),
-          “humidity: %0.2f %% " % (bme280_data.humidity),
-          "pressure: %0.2f Pa" % (bme280_data.pressure))
-    sleep(10)
+import bme280
+import smbus2
+from time import sleep
+
+port = 1
+address = 0x77 # Adafruit BME280 address. Other BME280s may be different
+bus = smbus2.SMBus(port)
+
+bme280.load_calibration_params(bus,address)
+
+while True:
+    bme280_data = bme280.sample(bus,address)
+    humidity  = bme280_data.humidity
+    pressure  = bme280_data.pressure
+    ambient_temperature = bme280_data.temperature
+    print(humidity, pressure, ambient_temperature)
+    sleep(1)
