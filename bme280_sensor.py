31340561fd4333477dbede611c41ee3bc3db55c6
xiaolinyang927 <54749402+xiaolinyang927@users.noreply.github.com>
2019-12-08 17:06:51 -0500

    Create bme280_sensor.py

---
+++ bme280_sensor.py
@@ -0,0 +1,16 @@
+import bme280
+import smbus2
+from time import sleep
+
+port = 1
+address = 0x77
+bus = smbus2.SMBus(port)
+
+bme280.load_calibration_params(bus, address)
+
+def get_bme280data():
+    bme280_data = bme280.sample(bus,address)
+    print("temperature: %0.2f C" % (bme280_data.temperature), 
+          "humidity: %0.2f %% " % (bme280_data.humidity),
+          "pressure: %0.2f Pa" % (bme280_data.pressure))
+    return bme280_data
