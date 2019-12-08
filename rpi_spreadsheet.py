fadcd155e735ca00db0ff6b27d060bf7b61a0bcc
xiaolinyang927 <54749402+xiaolinyang927@users.noreply.github.com>
2019-12-08 16:59:30 -0500

    Create rpi_spreadsheet.py

---
+++ rpi_spreadsheet.py
@@ -0,0 +1,51 @@
+import json
+import sys
+import time
+import datetime
+import gspread
+import psutil
+import subprocess
+from system_info import get_temperature
+from bme280_sensor import get_bme280data
+from oauth2client.service_account import ServiceAccountCredentials
+GDOCS_OAUTH_JSON       = 'rpidata-261419-471b1c5c5e6f.json'
+GDOCS_SPREADSHEET_NAME = 'rpidata'
+FREQUENCY_SECONDS      = 10
+def login_open_sheet(oauth_key_file, spreadsheet):
+    try:
+        credentials = ServiceAccountCredentials.from_json_keyfile_name(oauth_key_file, 
+                      scopes = ['https://spreadsheets.google.com/feeds',
+                                'https://www.googleapis.com/auth/drive'])
+        gc = gspread.authorize(credentials)
+        worksheet = gc.open(spreadsheet).sheet1
+        return worksheet
+    except Exception as ex:
+        print('Unable to login and get spreadsheet. Check OAuth credentials, spreadsheet name, and')
+        print('make sure spreadsheet is shared to the client_email address in the OAuth .json file!')
+        print('Google sheet login failed with error:', ex)
+        sys.exit(1)
+print('Logging sensor measurements to {0} every {1} seconds.'.format(GDOCS_SPREADSHEET_NAME, FREQUENCY_SECONDS))
+print('Press Ctrl-C to quit.')
+worksheet = None
+while True:
+    if worksheet is None:
+        worksheet = login_open_sheet(GDOCS_OAUTH_JSON, GDOCS_SPREADSHEET_NAME)
+    dat = datetime.datetime.now()
+    cpu = psutil.cpu_percent()
+    tmp = get_temperature()
+    bme280 = get_bme280data()
+    print(dat)
+    print('CPU Usage in %: '+str(cpu))
+    print('Temperature in C: ' +str(tmp))
+    try:
+        worksheet.append_row((str(dat), cpu, tmp, bme280.temperature, bme280.humidity, bme280.pressure))
+#        worksheet.append_row((dat, cpu, tmp))
+# gspread==0.6.2
+# https://github.com/burnash/gspread/issues/511
+    except:
+        print('Append error, logging in again')
+        worksheet = None
+        time.sleep(FREQUENCY_SECONDS)
+        continue
+    print('Wrote a row to {0}'.format(GDOCS_SPREADSHEET_NAME))
+    time.sleep(FREQUENCY_SECONDS)
